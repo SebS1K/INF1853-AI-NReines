e0847cb03180e2404abf2737fa39ef0f6d4e4447
SebS1K <hockeyseb93@gmail.com>
2026-03-21 18:32:48 -0400

    Create echiquier.py
    Échiquier se trouve ici, modifications à faire pour qu'il fonctionne avec algo génétique et hill climbing possiblement aussi

---
+++ echiquier.py
@@ -0,0 +1,42 @@
+class Echiquier:
+    def __init__(self, t):
+
+     # Initialiser l'échiquier avec la taille spécifiée
+        self.taille = t
+        self.tableau = [[False for _ in range(t)] for _ in range(t)]
+
+    def placerReine(self, ligne, colone):
+        self.tableau[ligne][colone] = True
+
+    def enleverReine(self, ligne, colone):
+        self.tableau[ligne][colone] = False
+
+    def estPositionValide(self, ligne, colone):
+        # Vérifier ligne
+        for i in range(self.taille):
+            if self.tableau[ligne][i]:
+                return False
+
+            # Vérifier diagonale gauche croissante
+        i, j = ligne, colone
+        while i >= 0 and j >= 0:
+            if self.tableau[i][j]:
+                return False
+            i -= 1
+            j -= 1
+
+            # Vérifier diagonale droite croissante
+        i, j = ligne, colone
+        while i >= 0 and j < self.taille:
+            if self.tableau[i][j]:
+                return False
+            i -= 1
+            j += 1
+
+        return True
+
+    def printEchiquierTexte(self):
+        for i in range(self.taille):
+            for j in range(self.taille):
+                print("X" if self.tableau[i][j] else "0", end="")
+            print()
